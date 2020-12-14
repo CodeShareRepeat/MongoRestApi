4a8aad6b2a694c30b4ed2efa9482ece8686f8d63
CodeShareRepeat <rckoenig@gmx.de>
2020-12-14 08:37:34 +0100

    add swagger support
    add swagger support.

--- Startup.cs
+++ Startup.cs
@@ -32,11 +32,24 @@ namespace MongoRestApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MongoRestApi", Version = "v1" });
             });
+
+        services.AddSwaggerGen();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
